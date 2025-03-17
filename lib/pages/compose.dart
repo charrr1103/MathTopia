e5dc3b3a1923f748ffba07fb17ad1a06369d9d4d
Charmaine Hooi <charmainehooi.1119@gmail.com>
2025-03-18 00:27:40 +0800

    Initial commit

--- lib/pages/compose.dart
+++ lib/pages/compose.dart
@@ -164,7 +164,7 @@ class _ComposePageState extends State<ComposePage> {
                           _buildDropTarget(1),
                         ],
                       ),
-                      const SizedBox(height: 100),
+                      const SizedBox(height: 105),
                       ElevatedButton(
                         onPressed: _clearSelection,
                         style: ElevatedButton.styleFrom(
